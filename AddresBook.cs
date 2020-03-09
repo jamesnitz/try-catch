6680d7e1d2fcb3433057adc9f87a711620300df7
jamesnitz <jamesanitz2@gmail.com>
2020-03-09 15:34:46 -0500

    completed exercise

---
+++ AddresBook.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace tryCatch
+{
+  public class AddressBook
+  {
+    public Dictionary<string, Contact> contacts = new Dictionary<string, Contact>();
+    public void AddContact(Contact contact)
+    {
+      contacts.Add(contact.Email, contact);
+    }
+
+    public Contact GetByEmail(string email)
+    {
+      var contact = contacts.FirstOrDefault(contact => contact.Key == email);
+      return contact.Value;
+    }
+
+  }
+}
